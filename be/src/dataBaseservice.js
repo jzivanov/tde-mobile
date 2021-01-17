4447e4a30dec17585a7b7948f421516f0cf8c7e2
Konstantin Sabo <kolesabo552@gmail.com>
2021-01-17 22:56:15 +0100

    nfowenlfwndfnowen

---
+++ be/src/dataBaseservice.js
@@ -0,0 +1,19 @@
+﻿const { v4: uuidv4 } = require('uuid');
+const items = [];
+function getById(id) {
+    console.log(items);
+    for (const item of items) {
+        if (item.id === id) {
+            return item;
+        }
+    }
+}
+function getAll() {
+    return items;
+}
+function save(newItem){
+    newItem.id=uuidv4();
+    items.push(newItem);
+
+}
+module.exports = {dbGetById:getById, save, getAll }
--- be/src/index.js
+++ be/src/index.js
@@ -3,19 +3,29 @@ const app = express()
 const port = 3000
 const cors = require('cors')
 const bodyParser = require('body-parser');
+const { dbGetById, save, getAll} = require('./dataBaseservice');
 
 app.use(cors())
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
 app.get('/', (req, res) => {
-    console.log(req)
-    res.send('Hello World!')
+    res.send(getAll())
+})
+app.get('/:id', (req, res) => {
+    console.log(req.params.id)
+    res.send(dbGetById(req.params.id))
 })
 app.post("/save",(req,res) => {
-    
-    console.log('Got body:', req.body);
+    console.log('Got body:', req.body.firstName);
+    console.log('Got body:', req.body.lastName);
+    save(req.body);
     res.sendStatus(200);
+});
+app.put('/:id', (req, res) => {
+});
+app.delete('/:id',(req,res) => {
+
 })
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
