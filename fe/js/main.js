b5614768eb5f58d8dabad33049dd23aad9d11944
Konstantin Sabo <kolesabo552@gmail.com>
2021-01-28 13:51:34 +0100

    add row,delete row

--- fe/js/main.js
+++ fe/js/main.js
@@ -4,14 +4,37 @@
 $(document).ready(function () {
   $('.table_items').click(function () {
     console.log(this);
-    const $row = $(this).closest("tr"),       // Finds the closest row <tr>
-      $tds = $row.find("td");             // Finds all children <td> elements
+    const $row = $(this).closest("tr"),       
+      $tds = $row.find("td");             
 
-    $.each($tds, function() {
-      if ($(this).attr("item_type").text() === "firstName") {
-        const firstName = $(this).text();
-        $('#ime').val(firstName)
-      }
+    $.each($tds, function () {
+        const item_type = $(this).attr("item_type");
+        if (typeof item_type !== typeof undefined && item_type !== false) {
+            if (item_type === 'firstName') {
+                $('#ime').val($(this).html())
+            }
+            if (item_type === 'lastName') {
+                $('#prezime').val($(this).html())
+            }
+            if (item_type === 'phoneNumber') {
+                $('#brtel').val($(this).html())
+            }
+            if (item_type === 'phone') {
+                $('#tel').val($(this).html())
+            }
+            if (item_type === 'phoneModel') {
+                $('#modeltel').val($(this).html())
+            }
+            if (item_type === 'IMEI') {
+                $('#IMEI').val($(this).html())
+            }
+            if (item_type === 'price') {
+                $('#cena').val($(this).html())
+            }
+            if (item_type === 'usluga') {
+                $('#uslugaKvar').val($(this).html())
+            }
+        }
     });
   });
   $("#saveButton").click(function () {
@@ -37,4 +60,33 @@ $(document).ready(function () {
       }
     });
   });
+});
+$("#saveButton").click(function () {
+    var tbody = $('#myTable').children('tbody');
+    var table = tbody.length ? tbody : $('#myTable');
+    var row = '<tr>'+
+        '<td>{{Ime}}</td>'+
+        '<td>{{Prezime}}</td>'+
+        '<td>{{Broj telefon}}</td>'+
+        '<td>{{Telefon}}</td>'+
+        '<td>{{Model}}</td>'+
+        '<td>{{IMEI}}</td>'+
+        '<td>{{Cena}}</td>'+
+        '<td>{{Usluga/kvar}}</td>'+
+
+
+
+    table.append(row.compose({
+        'Ime': $("#ime"),
+        'Prezime': $("#prezime"),
+        'Broj telefona': $("#brtel"),
+        'Telefon': $("#tel"),
+        'Model':$("#modelTel"),
+        'IMEI':$("#IMEI"),
+        'Cena':$("#cena"),
+        'Usluga/kvar':$("#usluga/kvar"),
+    }));
+});
+$('#myTable').on('click', buttonSelector='deleteButton', function () {
+    $(this).closest('tr').remove();
 });
